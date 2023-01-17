39bbbb6d65c58dde2052a43f997455813f319c73
Wan1129 <106945621+hiuhou-wan@users.noreply.github.com>
2023-01-17 09:52:47 +0900

    Hit関数

--- O-team/O-team/Player.h
+++ O-team/O-team/Player.h
@@ -1,4 +1,7 @@
 #include"DxLib.h"
+#include"flying_Armor.h"
+#include"flying_Attack.h"
+#include"flying_object.h"
 
 #pragma once
 class Player{
@@ -14,10 +17,11 @@ public:
 	virtual~Player();
 
 	void Update();
-
 	void InitPad();
 	void Draw();
-
 	int GetX() { return (int)X; }
 	int GetY() { return (int)Y; }
+
+	bool Hit(Flying_Armor armor);
+	bool Hit(Flying_Attack attack);
 };
